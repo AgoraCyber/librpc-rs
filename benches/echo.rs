fd23dd36ea803a27cac5c2a9057ced7630ac1bd2
yayanyang <yayanyang@gmail.com>
2023-05-08 13:43:35 +0800

    update

--- benches/echo.rs
+++ benches/echo.rs
@@ -1,66 +1,66 @@
-use std::thread::spawn;
+// use std::thread::spawn;
 
-use async_timer_rs::hashed::Timeout;
-use criterion::{async_executor::FuturesExecutor, *};
-use futures::{
-    channel::mpsc::{Receiver, SendError},
-    executor::block_on,
-    StreamExt,
-};
-use librpc::{dispatcher::Dispatcher, responder::Responder};
-use thiserror::Error;
+// use async_timer_rs::hashed::Timeout;
+// use criterion::{async_executor::FuturesExecutor, *};
+// use futures::{
+//     channel::mpsc::{Receiver, SendError},
+//     executor::block_on,
+//     StreamExt,
+// };
+// use librpc::{dispatcher::Dispatcher, responder::Responder};
+// use thiserror::Error;
 
-#[derive(Debug, Error)]
-enum TestError {
-    #[error(transparent)]
-    SendError(#[from] SendError),
+// #[derive(Debug, Error)]
+// enum TestError {
+//     #[error(transparent)]
+//     SendError(#[from] SendError),
 
-    #[error(transparent)]
-    IO(#[from] std::io::Error),
-}
+//     #[error(transparent)]
+//     IO(#[from] std::io::Error),
+// }
 
-async fn echo(
-    mut receiver: Receiver<(Option<u64>, String)>,
-    responder: Responder<String, TestError>,
-) {
-    let mut i = 0;
+// async fn echo(
+//     mut receiver: Receiver<(Option<u64>, String)>,
+//     responder: Responder<String, TestError>,
+// ) {
+//     let mut i = 0;
 
-    while let Some((id, msg)) = receiver.next().await {
-        i += 1;
+//     while let Some((id, msg)) = receiver.next().await {
+//         i += 1;
 
-        responder.complete(id.unwrap(), Ok(msg));
-    }
+//         responder.complete(id.unwrap(), Ok(msg));
+//     }
 
-    log::debug!("echo server exit with counter: {}", i)
-}
+//     log::debug!("echo server exit with counter: {}", i)
+// }
 
-async fn client(mut dispatcher: Dispatcher<String, String, TestError>) {
-    let echo = dispatcher
-        .call::<Timeout>(0, "hello".to_owned(), None)
-        .await
-        .unwrap()
-        .await
-        .unwrap();
+// async fn client(mut dispatcher: Dispatcher<String, String, TestError>) {
+//     let echo = dispatcher
+//         .call::<Timeout>(0, "hello".to_owned(), None)
+//         .await
+//         .unwrap()
+//         .await
+//         .unwrap();
 
-    assert_eq!(echo, "hello");
-}
+//     assert_eq!(echo, "hello");
+// }
 
-fn bench_rpc(c: &mut Criterion) {
-    _ = pretty_env_logger::try_init();
+// fn bench_rpc(c: &mut Criterion) {
+//     _ = pretty_env_logger::try_init();
 
-    let (dispatcher, receiver) = Dispatcher::new(100);
+//     let (dispatcher, receiver) = Dispatcher::new(100);
 
-    let responder = dispatcher.responder.clone();
+//     let responder = dispatcher.responder.clone();
 
-    spawn(move || block_on(echo(receiver, responder)));
+//     spawn(move || block_on(echo(receiver, responder)));
 
-    c.bench_function("echo rpc", |b| {
-        b.to_async(FuturesExecutor)
-            .iter(|| client(dispatcher.clone()))
-    });
+//     c.bench_function("echo rpc", |b| {
+//         b.to_async(FuturesExecutor)
+//             .iter(|| client(dispatcher.clone()))
+//     });
 
-    log::debug!("exit bench_rpc");
-}
+//     log::debug!("exit bench_rpc");
+// }
 
-criterion_group!(benches, bench_rpc);
-criterion_main!(benches);
+// criterion_group!(benches, bench_rpc);
+// criterion_main!(benches);
---
+++ src/channel.rs
@@ -0,0 +1,7 @@
+//! RPC channel types.
+
+/// Channel client side
+pub struct Client {}
+
+/// Channel server side.
+pub struct Server {}
